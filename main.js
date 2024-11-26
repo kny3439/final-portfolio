cc5f54e45c390b153bb64dd645a979ca53ea0ba0
김나연 <kny8664@naver.com>
2024-11-26 10:55:21 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import { menu } from "./src/js/menu.js";
+import { link } from "./src/js/link.js";
+import { project } from "./src/js/project.js";
+
+
+window.addEventListener('load', function () {
+    menu()
+    link()
+    project()
+})
+
---
+++ src/js/project.js
@@ -0,0 +1,40 @@
+export function project() {
+    var LslideSwiper = new Swiper("#project .project_slide_l", {
+        effect: 'fade',
+        loop: true,
+        speed: 1000,
+        slideActiveClass: 'on',
+        allowTouchMove: false,
+        autoplay: {
+            delay: 3200,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '#project .slide_arrow .next',
+            prevEl: '#project .slide_arrow .prev',
+        },
+    });
+    var RslideSwiper = new Swiper("#project .project_slide_r", {
+        loop: true,
+        speed: 1000,
+        slidesPerView: '3',
+        spaceBetween: 40,
+        allowTouchMove: false,
+        autoplay: {
+            delay: 3200,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '#project .slide_arrow .next',
+            prevEl: '#project .slide_arrow .prev',
+        },
+    });
+}
+
+$('.project-mainmenu').click(function () {
+    $('.project-submenu').stop().slideDown()
+})
+$('.project-mainmenu ').click(function () {
+    $('.project-submenu').stop().slideUp()
+    $(this).addClass('on')
+})

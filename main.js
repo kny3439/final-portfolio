121e3fcb42aea760931dd061bd5617af35d310e0
김나연 <kny8664@naver.com>
2024-11-27 09:28:04 +0900

    pf

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import { menu } from "./src/js/menu.js";
 import { link } from "./src/js/link.js";
 import { project } from "./src/js/project.js";
+import { bgColor} from "./src/js/bgColor.js";
 
 
 window.addEventListener('load', function () {
     menu()
     link()
     project()
+    bgColor()
 })
 
